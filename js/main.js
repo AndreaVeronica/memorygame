56444945b37cf7d63eeecba894a6b4b37e96a9dd
Andrea Airall <andreaairall@gmail.com>
2017-01-27 12:43:23 -0500

    initialized project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//on click flip piece over
+//only allow user to click 2 cards
+//if cards match increase score, remove cards.
+//if cards don't match, flip cards and switch player
+//when last cards gets matched, display winner
+//keep over all score
+//reset board
+
+
+
+
+
+// future iterations:
+//---- randomize board order
