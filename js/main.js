1a0adae5c6d4e161b2fc4006e0826ee5a33daf3c
Andrea Airall <andreaairall@gmail.com>
2017-02-01 11:10:17 -0500

    basic board built

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+$(document).ready(function() {
+
+  var boardContent = ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8" ];
+  var boardValue = [ ];
+  var boardIds = [ ];
+
+//create board
+function createBoard(){
+   console.log("called createBoard");
+    var displayBoard = "";
+     console.log(displayBoard);
+        for (var i = 0; i < boardContent.length; i++){
+          displayBoard += '<div class="pieces" id="piece'+ i+'">  </div>'
+          console.log(displayBoard);
+        }
+      document.getElementById('board').innerHTML = displayBoard;
+      console.log(displayBoard);
+   }
+
+createBoard();
+
 //on click flip piece over
 //only allow user to click 2 cards
 //if cards match increase score, remove cards.
@@ -12,3 +33,19 @@
 
 // future iterations:
 //---- randomize board order
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // closing doc.ready function
+});
