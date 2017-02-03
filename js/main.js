24c0f6b53266ac531f77c1fa5e454d0d836a4a91
Andrea Airall <andreaairall@gmail.com>
2017-02-03 10:03:38 -0500

    saving whats working now

--- js/main.js
+++ js/main.js
@@ -1,25 +1,122 @@
 $(document).ready(function() {
 
-  var boardContent = ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8" ];
+  var boardContent = ["1","1","2","2","3","3"];
   var boardValue = [ ];
+  var cardContent = [ ];
   var boardIds = [ ];
+  var counter = 0;
+
+
+//shuffle array content
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
 
 //create board
 function createBoard(){
-   console.log("called createBoard");
-    var displayBoard = "";
-     console.log(displayBoard);
-        for (var i = 0; i < boardContent.length; i++){
-          displayBoard += '<div class="pieces" id="piece'+ i+'">  </div>'
-          console.log(displayBoard);
-        }
-      document.getElementById('board').innerHTML = displayBoard;
-      console.log(displayBoard);
-   }
+    var saveShuffleArray = shuffleArray(boardContent);
+     cardContent = [];
+    var displayBoard = " ";
+        boardValue = [];
+        for (var i = 0; i < saveShuffleArray.length; i++){
+            boardValue.push(saveShuffleArray[i]);
+            displayBoard += '<div class="pieces" id="piece'+ i+'" >  </div>';
+            cardContent.push(displayBoard);
+
+      };
+    document.getElementById('board').innerHTML = displayBoard;
+
+}
 
 createBoard();
 
+// function flipCard(){
+//            console.log("clicked");
+//           }
+
+
+$('.pieces').click(function() {
+   flipCard(this.id, boardValue);
+
+
+ })
+
+    //  console.log(pieceId);
+
+    //  // for (var i = 0; i < boardValue.length; i++){
+    //  //    saveId = boardValue[i];
+
+    //  //
+    // console.log(boardValue);
+
+
+// var $gameCells = $('.pieces');
+
+
+function flipCard(card, content){
+  console.log(card);
+  console.log(content);
+
+
+
+
+// for (var i = 0; i < content.length; i++){
+
+  // }
+
+
+  // if (card == 'piece0'){
+  //       console.log(content[0]);
+  //     } else if (card == 'piece1'){
+  //        console.log(content[1]);
+  //     }
+
+
+  // }
+
+
+  // for (var i = 0; i < content.length; i++){
+
+  // }
+
+  // content.forEach(assignValue);
+
+  // function assignValue(item, index){
+  //    console.log(item, index)
+  //    // var pieceIndex = 'piece'+index;
+  //    if (index == 0){
+  //     console.log("worked");
+  //    }
+
+  // }
+  // for (var i = 0; i < content.length; i++){
+
+  // if (card == 'piece0'){
+  //   console.log(content[i]);
+  // } else if (card == 'piece1'){
+
+  // }
+
+
+
+
+}
+
+
+// });
+
+
+
+
 //on click flip piece over
+
+
 //only allow user to click 2 cards
 //if cards match increase score, remove cards.
 //if cards don't match, flip cards and switch player
