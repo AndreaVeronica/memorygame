c6e2705eba606af2a2b1e19ffbf0ade93997e86c
Andrea Airall <andreaairall@gmail.com>
2017-02-06 14:09:25 -0500

    game finished

---
+++ README.md
@@ -0,0 +1,36 @@
+## Tic-Tac-Toe
+
+### App Info
+
+A web app that allows users to play a memory card game with disney quotes.
+
+
+### Technologies Used
+
+- HTML
+- CSS
+- JavaScript/jQuery
+
+
+
+### Cheatsheet
+
+
+-  Grandmother Willow
+ - "Sometimes the right path is not the easiest one.
+
+- The Emporor
+- "The flower that blooms in adversity is the most rare and beautiful of them all."
+
+- Rafiki
+- "The past can hurt. But the way I see it, you can either run from it, or learn from it.
+
+- Pocahontas
+- "If you walk the footsteps of a stranger, you'll learn things you never knew you never knew."
+
+- Zeus
+- "A true hero isn't measured by the size of his strength, but by the strength of his heart."
+
+- Mufasa
+- "I'm only brave when I have to be. Being brave doesn't mean you go looking for trouble."
+
--- js/main.js
+++ js/main.js
@@ -32,33 +32,39 @@ var mov6 =
         movie: 'Pocahontas',
      };
 var char1 =
-     { number: 1,
-         speaker: 'The Emporor',
+     {   number: 1,
+         speaker: 'The Emperor',
+         short: 'emperor',
          pic: 'http://images6.fanpop.com/image/polls/1202000/1202219_1365371420033_full.jpg',
      };
 var char2 =
      {   number: 2,
          speaker: 'Rafiki',
+         short: 'rafiki',
          pic: 'https://a.dilcdn.com/bl/wp-content/uploads/sites/25/2015/01/Rafiki-The-Lion-King-3.jpeg',
      };
 var char3 =
      {   number: 3,
          speaker: 'Pocahontas',
+         short: 'pocahontas2',
          pic: 'http://vignette1.wikia.nocookie.net/characters/images/3/32/Pocahontas1.jpg/revision/latest?cb=20150601000252',
      };
 var char4 =
      {  number: 4,
          speaker: 'Zeus',
+         short: 'zeus',
          pic: 'http://66.media.tumblr.com/tumblr_l6cph3rEMG1qd0axho1_1280.jpg',
      };
 var char5 =
      {  number: 5,
          speaker: 'Mufasa',
+         short: 'mufasa',
          pic: 'http://vignette1.wikia.nocookie.net/tlk-characters/images/1/17/Mufasa_TLK1Presentati.jpg/revision/latest?cb=20140801170201',
      };
 var char6 =
      {  number: 6,
          speaker: 'Grandmother Willow',
+         short: 'gmawillow',
          pic: 'http://images6.fanpop.com/image/photos/36400000/Walt-Disney-Characters-image-walt-disney-characters-36400800-5760-3240.jpg',
      };
 
@@ -140,13 +146,16 @@ function flipCard(card, content){
                        var h3El = document.createElement("h3");
                        var imgEl = document.createElement("IMG");
                        var speakerContent = document.createTextNode(content[i].speaker);
-                       var picContent = document.createTextNode(content[i].pic);
+                       var short = content[i].short;
+                       //var picContent = document.createTextNode(content[i].pic)
+                       imgEl.src = 'images/'+ short + '.jpg';
                        h3El.appendChild(speakerContent);
-                       imgEl.appendChild(picContent);
+                     // imgEl.appendChild(picContent);
                        div.appendChild(h3El);
                        div.appendChild(imgEl);
-                      h3El.classList.add("speaker-style");
-                      imgEl.classList.add("pic-style");
+                       h3El.classList.add("speaker-style");
+                       // div.classList.add(content[i].short);
+                       imgEl.classList.add("pic-style");
                   }
                   turn.push(content[i].number);
                   console.log(turn);
@@ -174,12 +183,9 @@ function clearDiv(){
           setTimeout(timer, 1000);
           function timer(){
                var div1 = document.getElementById(saveCardIndex0);
-             //  $(div1).removeClass("clicked");
                div1.innerHTML = " ";
                $(div1).on('click', startGame);
-              // $(div1).addClass("clickable");
                var div2 = document.getElementById(saveCardIndex1);
-              //  $(div2).removeClass("clicked");
                div2.innerHTML = " ";
                $(div2).on('click', startGame);
 
@@ -188,19 +194,11 @@ function clearDiv(){
 
 // limit clicks to only 2 cards, then clear board.
 function checkForMatch(card, content){
-     // var saveIt = div;
-     // console.log(saveIt);
       if (turn[0] == turn[1]){
-              console.log("its a match");
+              //console.log("its a match");
               matchMade(cardIds, turn[0],turn[1]);
             } else{
-              // $(div).removeClass("clicked");
                clearDiv();
-               // console.log(this.id);
-
-
-               // var div = document.getElementById();
-               // $(div1).removeClass("clicked");
             }
 }
 
@@ -208,16 +206,14 @@ function checkForMatch(card, content){
 //if cards match increase score, remove cards.
 //when last cards gets matched, display "you've won"
 function matchMade(cards, num1, num2){
-      // console.log(cards);
-
       if (num1 == num2){
         console.log("match has been made");
+        score++;
+              if (score >= 6){
+               var win = document.getElementById('winner');
+               win.innerHTML = 'Yay! You won!';
 
-      }
-      score++;
-      console.log(score);
-      if (score >= 6){
-         alert("you win");
+            }
       }
 }
 
@@ -237,45 +233,6 @@ function matchMade(cards, num1, num2){
 
 
 
-
-// var quotes = [
-//      {   text: 'The flower that blooms in adversity is the most rare and beautiful of them all.',
-//          number: 1,
-//          speaker: 'The Emporor',
-//          movie: 'Mulan',
-//          pic: 'http://images6.fanpop.com/image/polls/1202000/1202219_1365371420033_full.jpg',
-//      },
-//      {   text: 'The past can hurt. But the way I see it, you can either run from it, or learn from it.',
-//          number: 2,
-//          speaker: 'Rafiki',
-//          movie: 'The Lion King',
-//          pic: 'https://a.dilcdn.com/bl/wp-content/uploads/sites/25/2015/01/Rafiki-The-Lion-King-3.jpeg',
-//      },
-//      {   text: 'If you walk the footsteps of a stranger, you\'ll learn things you never knew you never knew.',
-//          number: 3,
-//          speaker: 'Pocahontas',
-//          movie: 'Pocahontas',
-//          pic: 'http://vignette1.wikia.nocookie.net/characters/images/3/32/Pocahontas1.jpg/revision/latest?cb=20150601000252',
-//      },
-//      {   text: 'A true hero isn\'t measured by the size of his strength, but by the strength of his heart.',
-//          number: 4,
-//          speaker: 'Zeus',
-//          movie: 'Herculus',
-//          pic: 'http://66.media.tumblr.com/tumblr_l6cph3rEMG1qd0axho1_1280.jpg',
-//      },
-//      {   text: 'I\'m only brave when I have to be. Being brave doesn\'t mean you go looking for trouble.',
-//          number: 5,
-//          speaker: 'Mufasa',
-//          movie: 'The Lion King',
-//          pic: 'http://vignette1.wikia.nocookie.net/tlk-characters/images/1/17/Mufasa_TLK1Presentati.jpg/revision/latest?cb=20140801170201',
-//      } ,
-//      {   text: 'Sometimes the right path is not the easiest one.',
-//          number: 6,
-//          speaker: 'Grandmother Willow',
-//          movie: 'Pocahontas',
-//          pic: 'http://images6.fanpop.com/image/photos/36400000/Walt-Disney-Characters-image-walt-disney-characters-36400800-5760-3240.jpg',
-//      }
-//     ];
 
 
 
