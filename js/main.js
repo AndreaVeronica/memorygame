14cc5e048a29d6931be7aa6098998a6d6d3e124f
Andrea Airall <andreaairall@gmail.com>
2017-02-05 21:25:58 -0500

    game working with correct data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,70 @@
 $(document).ready(function() {
 
-  var boardContent = ["1","1","2","2","3","3","4","4","5","5","6","6"];
+var mov1 =
+     {  number: 1,
+         text: 'The flower that blooms in adversity is the most rare and beautiful of them all.',
+         movie: 'Mulan'
+     };
+var mov2 =
+     {   number: 2,
+        text: 'The past can hurt. But the way I see it, you can either run from it, or learn from it.',
+         movie: 'The Lion King',
+
+     };
+var mov3 =
+     {  number: 3,
+        text: 'If you walk the footsteps of a stranger, you\'ll learn things you never knew you never knew.',
+         movie: 'Pocahontas',
+     };
+var mov4 =
+     { number: 4,
+         text: 'A true hero isn\'t measured by the size of his strength, but by the strength of his heart.',
+         movie: 'Herculus',
+     };
+var mov5 =
+     {  number: 5,
+        text: 'I\'m only brave when I have to be. Being brave doesn\'t mean you go looking for trouble.',
+         movie: 'The Lion King',
+     };
+var mov6 =
+     {  number: 6,
+       text: 'Sometimes the right path is not the easiest one.',
+        movie: 'Pocahontas',
+     };
+var char1 =
+     { number: 1,
+         speaker: 'The Emporor',
+         pic: 'http://images6.fanpop.com/image/polls/1202000/1202219_1365371420033_full.jpg',
+     };
+var char2 =
+     {   number: 2,
+         speaker: 'Rafiki',
+         pic: 'https://a.dilcdn.com/bl/wp-content/uploads/sites/25/2015/01/Rafiki-The-Lion-King-3.jpeg',
+     };
+var char3 =
+     {   number: 3,
+         speaker: 'Pocahontas',
+         pic: 'http://vignette1.wikia.nocookie.net/characters/images/3/32/Pocahontas1.jpg/revision/latest?cb=20150601000252',
+     };
+var char4 =
+     {  number: 4,
+         speaker: 'Zeus',
+         pic: 'http://66.media.tumblr.com/tumblr_l6cph3rEMG1qd0axho1_1280.jpg',
+     };
+var char5 =
+     {  number: 5,
+         speaker: 'Mufasa',
+         pic: 'http://vignette1.wikia.nocookie.net/tlk-characters/images/1/17/Mufasa_TLK1Presentati.jpg/revision/latest?cb=20140801170201',
+     };
+var char6 =
+     {  number: 6,
+         speaker: 'Grandmother Willow',
+         pic: 'http://images6.fanpop.com/image/photos/36400000/Walt-Disney-Characters-image-walt-disney-characters-36400800-5760-3240.jpg',
+     };
+
+var boardContent = [mov1, mov2, mov3, mov4, mov5, mov6, char1, char2, char3, char4, char5, char6];
+
+
   var boardValue = [ ];
   var cardContent = [ ];
   var boardIds = [ ];
@@ -36,6 +100,7 @@ function shuffleArray(array) {
 function createBoard(){
     var saveShuffleArray = shuffleArray(boardContent);
         cardContent = [];
+
     var displayBoard = " ";
         boardValue = [];
         for (var i = 0; i < saveShuffleArray.length; i++){
@@ -51,8 +116,8 @@ createBoard();
 $('.pieces').on('click', startGame);
 
  function startGame(){
-  // board, value
-     console.log(this.id);
+     // console.log(boardValue);
+      console.log(this.id);
      counter++;
      flipCard(this.id, boardValue);
 }
@@ -60,13 +125,18 @@ $('.pieces').on('click', startGame);
 //on click "flip card" - render data.
 
 function flipCard(card, content){
-     console.log(content);
+    console.log(content);
      var div = document.getElementById(card);
-
+   //  console.log(div);
      for (var i = 0; i < content.length; i++){
           if (card == "piece"+[i]){
-                  div.innerHTML = div.innerHTML + content[i];
-                  turn.push(content[i]);
+                  if (content[i].text) {
+                    div.innerHTML = div.innerHTML + content[i].text;
+                  } else if (content[i].speaker) {
+                      div.innerHTML = div.innerHTML + content[i].speaker;
+                      div.innerHTML = div.innerHTML + content[i].pic;
+                  }
+                  turn.push(content[i].number);
                   console.log(turn);
                   cardIds.push(card);
                   $(div).off('click');
@@ -135,7 +205,7 @@ function matchMade(cards, num1, num2){
       score++;
       console.log(score);
       if (score >= 6){
-         console.log("you win");
+         alert("you win");
       }
 }
 
@@ -156,7 +226,44 @@ function matchMade(cards, num1, num2){
 
 
 
-
+// var quotes = [
+//      {   text: 'The flower that blooms in adversity is the most rare and beautiful of them all.',
+//          number: 1,
+//          speaker: 'The Emporor',
+//          movie: 'Mulan',
+//          pic: 'http://images6.fanpop.com/image/polls/1202000/1202219_1365371420033_full.jpg',
+//      },
+//      {   text: 'The past can hurt. But the way I see it, you can either run from it, or learn from it.',
+//          number: 2,
+//          speaker: 'Rafiki',
+//          movie: 'The Lion King',
+//          pic: 'https://a.dilcdn.com/bl/wp-content/uploads/sites/25/2015/01/Rafiki-The-Lion-King-3.jpeg',
+//      },
+//      {   text: 'If you walk the footsteps of a stranger, you\'ll learn things you never knew you never knew.',
+//          number: 3,
+//          speaker: 'Pocahontas',
+//          movie: 'Pocahontas',
+//          pic: 'http://vignette1.wikia.nocookie.net/characters/images/3/32/Pocahontas1.jpg/revision/latest?cb=20150601000252',
+//      },
+//      {   text: 'A true hero isn\'t measured by the size of his strength, but by the strength of his heart.',
+//          number: 4,
+//          speaker: 'Zeus',
+//          movie: 'Herculus',
+//          pic: 'http://66.media.tumblr.com/tumblr_l6cph3rEMG1qd0axho1_1280.jpg',
+//      },
+//      {   text: 'I\'m only brave when I have to be. Being brave doesn\'t mean you go looking for trouble.',
+//          number: 5,
+//          speaker: 'Mufasa',
+//          movie: 'The Lion King',
+//          pic: 'http://vignette1.wikia.nocookie.net/tlk-characters/images/1/17/Mufasa_TLK1Presentati.jpg/revision/latest?cb=20140801170201',
+//      } ,
+//      {   text: 'Sometimes the right path is not the easiest one.',
+//          number: 6,
+//          speaker: 'Grandmother Willow',
+//          movie: 'Pocahontas',
+//          pic: 'http://images6.fanpop.com/image/photos/36400000/Walt-Disney-Characters-image-walt-disney-characters-36400800-5760-3240.jpg',
+//      }
+//     ];
 
 
 
