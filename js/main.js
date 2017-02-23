2d69b4435c70f3d2ec2fdb0944d7c56180f0e6fb
Andrea Airall <andreaairall@gmail.com>
2017-02-23 10:49:58 -0500

    cleaned game, made one var for all quote objects, added to quote bank, game logic working

--- js/main.js
+++ js/main.js
@@ -1,74 +1,70 @@
 $(document).ready(function() {
 
-    var mov1 = {
-        number: 1,
+    var quotes = [{
         text: 'The flower that blooms in adversity is the most rare and beautiful of them all.',
-        movie: 'Mulan'
-    };
-    var mov2 = {
-        number: 2,
-        text: 'The past can hurt. But the way I see it, you can either run from it, or learn from it.',
-        movie: 'The Lion King',
-
-    };
-    var mov3 = {
-        number: 3,
-        text: 'If you walk the footsteps of a stranger, you\'ll learn things you never knew you never knew.',
-        movie: 'Pocahontas',
-    };
-    var mov4 = {
-        number: 4,
-        text: 'A true hero isn\'t measured by the size of his strength, but by the strength of his heart.',
-        movie: 'Herculus',
-    };
-    var mov5 = {
-        number: 5,
-        text: 'I\'m only brave when I have to be. Being brave doesn\'t mean you go looking for trouble.',
-        movie: 'The Lion King',
-    };
-    var mov6 = {
-        number: 6,
-        text: 'Sometimes the right path is not the easiest one.',
-        movie: 'Pocahontas',
-    };
-    var char1 = {
         number: 1,
         speaker: 'The Emperor',
+        movie: 'Mulan',
         short: 'emperor',
         pic: 'http://images6.fanpop.com/image/polls/1202000/1202219_1365371420033_full.jpg',
-    };
-    var char2 = {
+    }, {
+        text: 'The past can hurt. But the way I see it, you can either run from it, or learn from it.',
         number: 2,
         speaker: 'Rafiki',
+        movie: 'The Lion King',
         short: 'rafiki',
         pic: 'https://a.dilcdn.com/bl/wp-content/uploads/sites/25/2015/01/Rafiki-The-Lion-King-3.jpeg',
-    };
-    var char3 = {
+    }, {
+        text: 'If you walk the footsteps of a stranger, you\'ll learn things you never knew you never knew.',
         number: 3,
         speaker: 'Pocahontas',
+        movie: 'Pocahontas',
         short: 'pocahontas2',
         pic: 'http://vignette1.wikia.nocookie.net/characters/images/3/32/Pocahontas1.jpg/revision/latest?cb=20150601000252',
-    };
-    var char4 = {
+    }, {
+        text: 'A true hero isn\'t measured by the size of his strength, but by the strength of his heart.',
         number: 4,
         speaker: 'Zeus',
         short: 'zeus',
+        movie: 'Herculus',
         pic: 'http://66.media.tumblr.com/tumblr_l6cph3rEMG1qd0axho1_1280.jpg',
-    };
-    var char5 = {
+    }, {
+        text: 'I\'m only brave when I have to be. Being brave doesn\'t mean you go looking for trouble.',
         number: 5,
         speaker: 'Mufasa',
+        movie: 'The Lion King',
         short: 'mufasa',
         pic: 'http://vignette1.wikia.nocookie.net/tlk-characters/images/1/17/Mufasa_TLK1Presentati.jpg/revision/latest?cb=20140801170201',
-    };
-    var char6 = {
+    }, {
+        text: 'Sometimes the right path is not the easiest one.',
         number: 6,
         speaker: 'Grandmother Willow',
+        movie: 'Pocahontas',
         short: 'gmawillow',
         pic: 'http://images6.fanpop.com/image/photos/36400000/Walt-Disney-Characters-image-walt-disney-characters-36400800-5760-3240.jpg',
-    };
+    }, {
+        text: 'You are braver than you believe, stronger than you seem, and smarter than you think.',
+        number: 7,
+        speaker: 'Christopher Robin',
+        movie: 'Winnie the Pooh',
+        short: 'chrisrob',
+        pic: '',
+    }, {
+        text: 'Life\'s not a spectator sport. If watchin\' is all you\'re gonna do, then you\'re gonna watch your life go by without ya',
+        number: 8,
+        speaker: 'Laverne',
+        movie: 'The Hunchback of Notre Dame',
+        short: 'laverne',
+        pic: '',
+    }, ];
+    // var quoteText = [];
+    // var quoteChar = [];
 
-    var boardContent = [mov1, mov2, mov3, mov4, mov5, mov6, char1, char2, char3, char4, char5, char6];
+    var boardContent = [];
+    var arrayOfQuotes = [];
+
+    var boardText = [];
+    var boardChar = [];
 
 
     var boardValue = [];
@@ -97,14 +93,30 @@ $(document).ready(function() {
         return array;
     }
 
+    function picQuotes() {
+        for (var i = 0; i < quotes.length; i++) {
+            arrayOfQuotes.push(quotes[i]);
+        }
+        shuffleArray(arrayOfQuotes);
+
+        for (var i = 0; i < 6; i++) {
+             boardContent.push(arrayOfQuotes[i].text);
+             boardContent.push(arrayOfQuotes[i]);
+        };
+        // console.log(boardContent);
+    }
+
+
     //create board
     function createBoard() {
+        picQuotes();
         shuffleArray(boardContent);
+        // console.log(boardContent);
         var displayBoard = " ";
         boardValue = [];
         for (var i = 0; i < boardContent.length; i++) {
-            boardValue.push(boardContent[i]);
-            displayBoard += '<div class="pieces" id="piece' + i + '" >  </div>';
+             boardValue.push(boardContent[i]);
+             displayBoard += '<div class="pieces" id="piece' + i + '" >  </div>';
         };
         document.getElementById('board').innerHTML = displayBoard;
 
@@ -116,7 +128,9 @@ $(document).ready(function() {
 
     function startGame() {
         counter++;
+        console.log(boardValue)
         flipCard(this.id, boardValue);
+
     }
 
     //on click "flip card" - render data.
@@ -126,13 +140,7 @@ $(document).ready(function() {
 
         for (var i = 0; i < content.length; i++) {
             if (card == "piece" + [i]) {
-                if (content[i].text) {
-                    var h3El = document.createElement("h3");
-                    var textContent = document.createTextNode(content[i].text);
-                    h3El.appendChild(textContent);
-                    div.appendChild(h3El);
-                    h3El.classList.add("text-style");
-                } else if (content[i].speaker) {
+                if (content[i].speaker) {
                     var h3El = document.createElement("h3");
                     var imgEl = document.createElement("IMG");
                     var speakerContent = document.createTextNode(content[i].speaker);
@@ -144,14 +152,18 @@ $(document).ready(function() {
                     h3El.classList.add("speaker-style");
                     // div.classList.add(content[i].short);
                     imgEl.classList.add("pic-style");
+                } else if (content[i]) {
+                    var h3El = document.createElement("h3");
+                    var textContent = document.createTextNode(content[i]);
+                    h3El.appendChild(textContent);
+                    div.appendChild(h3El);
+                    h3El.classList.add("text-style");
                 }
-                turn.push(content[i].number);
+                turn.push(content[i].text || content[i]);
                 console.log(turn);
                 cardIds.push(card);
                 $(div).off('click');
-            } else {
-                // console.log("nope");
-            }
+             }
         }
         if (counter >= 2) {
             counter = 0;
@@ -163,11 +175,12 @@ $(document).ready(function() {
         }
     }
 
- //when cards match remove click option, change visual
+    //when cards match remove click option, change visual
     function clearDiv(cards) {
-           var saveCardIndex0 = cards[0];
-           var saveCardIndex1 = cards[1];
+        var saveCardIndex0 = cards[0];
+        var saveCardIndex1 = cards[1];
         setTimeout(timer, 1000);
+
         function timer() {
             var div1 = document.getElementById(saveCardIndex0);
             div1.innerHTML = " ";
@@ -183,7 +196,6 @@ $(document).ready(function() {
     //if cards match increase score, remove cards.
     //when last cards gets matched, display "you've won"
     function checkForMatch(cards, content) {
-        console.log(cards + "testing");
         if (turn[0] == turn[1]) {
             console.log("match has been made");
             score++;
